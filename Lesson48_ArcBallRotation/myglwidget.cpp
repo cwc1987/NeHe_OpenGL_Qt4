e22e24eb059a39ff0efb83f84cb02c9e4ddeb446
cwc1987 <cwc1987@163.com>
2014-05-04 14:14:23 +0800

    Add Lesson48

--- Lesson48_ArcBallRotation/myglwidget.cpp
+++ Lesson48_ArcBallRotation/myglwidget.cpp
@@ -3,6 +3,107 @@
 //轨迹球实现的鼠标旋转
 //使用鼠标旋转物体,很简单也有很多实现方法,这里我们教会你模拟轨迹球来实现它.
 
+//Arcball sphere constants:
+//Diameter is       2.0f
+//Radius is         1.0f
+//Radius squared is 1.0f
+
+const float PI2 = 2.0*3.1415926535f;								// PI Squared
+
+void ArcBall_t::_mapToSphere(const Point2fT* NewPt, Vector3fT* NewVec) const
+{
+    Point2fT TempPt;
+    GLfloat length;
+
+    //Copy paramter into temp point
+    TempPt = *NewPt;
+
+    //Adjust point coords and scale down to range of [-1 ... 1]
+    TempPt.s.X  =        (TempPt.s.X * this->AdjustWidth)  - 1.0f;
+    TempPt.s.Y  = 1.0f - (TempPt.s.Y * this->AdjustHeight);
+
+    //Compute the square of the length of the vector to the point from the center
+    length      = (TempPt.s.X * TempPt.s.X) + (TempPt.s.Y * TempPt.s.Y);
+
+    //If the point is mapped outside of the sphere... (length > radius squared)
+    if (length > 1.0f)
+    {
+        GLfloat norm;
+
+        //Compute a normalizing factor (radius / sqrt(length))
+        norm    = 1.0f / FuncSqrt(length);
+
+        //Return the "normalized" vector, a point on the sphere
+        NewVec->s.X = TempPt.s.X * norm;
+        NewVec->s.Y = TempPt.s.Y * norm;
+        NewVec->s.Z = 0.0f;
+    }
+    else    //Else it's on the inside
+    {
+        //Return a vector to a point mapped inside the sphere sqrt(radius squared - length)
+        NewVec->s.X = TempPt.s.X;
+        NewVec->s.Y = TempPt.s.Y;
+        NewVec->s.Z = FuncSqrt(1.0f - length);
+    }
+}
+
+//Create/Destroy
+ArcBall_t::ArcBall_t(GLfloat NewWidth, GLfloat NewHeight)
+{
+    //Clear initial values
+    this->StVec.s.X     =
+    this->StVec.s.Y     =
+    this->StVec.s.Z     =
+
+    this->EnVec.s.X     =
+    this->EnVec.s.Y     =
+    this->EnVec.s.Z     = 0.0f;
+
+    //Set initial bounds
+    this->setBounds(NewWidth, NewHeight);
+}
+
+//Mouse down
+void ArcBall_t::click(const Point2fT* NewPt)
+{
+    //Map the point to the sphere
+    this->_mapToSphere(NewPt, &this->StVec);
+}
+
+//Mouse drag, calculate rotation
+void ArcBall_t::drag(const Point2fT* NewPt, Quat4fT* NewRot)
+{
+    //Map the point to the sphere
+    this->_mapToSphere(NewPt, &this->EnVec);
+    //Return the quaternion equivalent to the rotation
+    if (NewRot)
+    {
+        Vector3fT  Perp;
+
+        //Compute the vector perpendicular to the begin and end vectors
+        Vector3fCross(&Perp, &this->StVec, &this->EnVec);
+
+        //Compute the length of the perpendicular vector
+        if (Vector3fLength(&Perp) > Epsilon)    //if its non-zero
+        {
+            //We're ok, so return the perpendicular vector as the transform after all
+            NewRot->s.X = Perp.s.X;
+            NewRot->s.Y = Perp.s.Y;
+            NewRot->s.Z = Perp.s.Z;
+            //In the quaternion values, w is cosine (theta / 2), where theta is rotation angle
+            NewRot->s.W= Vector3fDot(&this->StVec, &this->EnVec);
+        }
+        else                                    //if its zero
+        {
+            //The begin and end vectors coincide, so return an identity transform
+            NewRot->s.X =
+            NewRot->s.Y =
+            NewRot->s.Z =
+            NewRot->s.W = 0.0f;
+        }
+    }
+}
+
 //轨迹球控制
 //By Terence J. Grant (tjgrant@tatewake.com)
 //如果只用鼠标来控制你的模型是不是很酷?轨迹球可以帮你做到这一点，我将告诉你我的实现，你可以把它应用在你的工程里。
@@ -30,82 +131,72 @@ Matrix3fT ThisRot = { 1.0f, 0.0f, 0.0f,
 //为了更新鼠标的移动范围，我们在函数ReshapeGL中加入下面一行：
 
 MyGLWidget::MyGLWidget(QWidget *parent) :
-    QGLWidget(parent), m_show_full_screen(false)
+    QGLWidget(parent), m_show_full_screen(false), m_arcBall(640.0f, 480.0f),
+    m_leftButtonClicked(false)
 {
     showNormal();
 }
 
 MyGLWidget::~MyGLWidget()
 {
+    gluDeleteQuadric(m_quadratic);
 }
 
-//下面的代码的作用是重新设置OpenGL场景的大小，而不管窗口的大小是否已经改变(假定您没有使用全屏模式)。
-//甚至您无法改变窗口的大小时(例如您在全屏模式下)，它至少仍将运行一次--在程序开始时设置我们的透视图。
-//OpenGL场景的尺寸将被设置成它显示时所在窗口的大小。
 void MyGLWidget::resizeGL(int w, int h)
 {
-    if(h == 0)// 防止被零除
-    {
-        h = 1;// 将高设为1
-    }
-    glViewport(0, 0, w, h); //重置当前的视口
-    //下面几行为透视图设置屏幕。意味着越远的东西看起来越小。这么做创建了一个现实外观的场景。
-    //此处透视按照基于窗口宽度和高度的45度视角来计算。0.1f，100.0f是我们在场景中所能绘制深度的起点和终点。
-    //glMatrixMode(GL_PROJECTION)指明接下来的两行代码将影响projection matrix(投影矩阵)。
-    //投影矩阵负责为我们的场景增加透视。 glLoadIdentity()近似于重置。它将所选的矩阵状态恢复成其原始状态。
-    //调用glLoadIdentity()之后我们为场景设置透视图。
-    //glMatrixMode(GL_MODELVIEW)指明任何新的变换将会影响 modelview matrix(模型观察矩阵)。
-    //模型观察矩阵中存放了我们的物体讯息。最后我们重置模型观察矩阵。如果您还不能理解这些术语的含义，请别着急。
-    //在以后的教程里，我会向大家解释。只要知道如果您想获得一个精彩的透视场景的话，必须这么做。
-    glMatrixMode(GL_PROJECTION);// 选择投影矩阵
-    glLoadIdentity();// 重置投影矩阵
-    //设置视口的大小
-    gluPerspective(45.0f,(GLfloat)w/(GLfloat)h,0.1f,100.0f);
+    glViewport (0, 0, (GLsizei)(w), (GLsizei)(h));				// Reset The Current Viewport
+    glMatrixMode (GL_PROJECTION);										// Select The Projection Matrix
+    glLoadIdentity ();													// Reset The Projection Matrix
+    gluPerspective (45.0f, (GLfloat)(w)/(GLfloat)(h),			// Calculate The Aspect Ratio Of The Window
+                    1.0f, 100.0f);
+    glMatrixMode (GL_MODELVIEW);										// Select The Modelview Matrix
+    glLoadIdentity ();													// Reset The Modelview Matrix
 
-    glMatrixMode(GL_MODELVIEW);	//选择模型观察矩阵
-    glLoadIdentity(); // 重置模型观察矩阵
+    m_arcBall.setBounds((GLfloat)w, (GLfloat)h);                 //*NEW* Update mouse bounds for arcball
 }
 
 //接下的代码段中，我们将对OpenGL进行所有的设置。我们将设置清除屏幕所用的颜色，打开深度缓存，启用smooth shading(阴影平滑)，等等。
 //这个例程直到OpenGL窗口创建之后才会被调用。此过程将有返回值。但我们此处的初始化没那么复杂，现在还用不着担心这个返回值。
 void MyGLWidget::initializeGL()
 {
-    //下一行启用smooth shading(阴影平滑)。阴影平滑通过多边形精细的混合色彩，并对外部光进行平滑。
-    //我将在另一个教程中更详细的解释阴影平滑。
-    glShadeModel(GL_SMOOTH); // 启用阴影平滑
+    glClearColor (0.0f, 0.0f, 0.0f, 0.5f);							// Black Background
+    glClearDepth (1.0f);											// Depth Buffer Setup
+    glDepthFunc (GL_LEQUAL);										// The Type Of Depth Testing (Less Or Equal)
+    glEnable (GL_DEPTH_TEST);										// Enable Depth Testing
+    glShadeModel (GL_FLAT);											// Select Flat Shading (Nice Definition Of Objects)
+    glHint (GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);				// Set Perspective Calculations To Most Accurate
 
-    //下一行设置清除屏幕时所用的颜色。如果您对色彩的工作原理不清楚的话，我快速解释一下。
-    //色彩值的范围从0.0f到1.0f。0.0f代表最黑的情况，1.0f就是最亮的情况。
-    //glClearColor 后的第一个参数是Red Intensity(红色分量),第二个是绿色，第三个是蓝色。
-    //最大值也是1.0f，代表特定颜色分量的最亮情况。最后一个参数是Alpha值。当它用来清除屏幕的时候，我们不用关心第四个数字。
-    //现在让它为0.0f。我会用另一个教程来解释这个参数。
-    //通过混合三种原色(红、绿、蓝)，您可以得到不同的色彩。希望您在学校里学过这些。
-    //因此，当您使用glClearColor(0.0f,0.0f,1.0f,0.0f)，您将用亮蓝色来清除屏幕。
-    //如果您用 glClearColor(0.5f,0.0f,0.0f,0.0f)的话，您将使用中红色来清除屏幕。不是最亮(1.0f)，也不是最暗 (0.0f)。
-    //要得到白色背景，您应该将所有的颜色设成最亮(1.0f)。要黑色背景的话，您该将所有的颜色设为最暗(0.0f)。
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // 黑色背景
+    m_quadratic=gluNewQuadric();										// Create A Pointer To The Quadric Object
+    gluQuadricNormals(m_quadratic, GLU_SMOOTH);						// Create Smooth Normals
+    gluQuadricTexture(m_quadratic, GL_TRUE);							// Create Texture Coords
 
-    //接下来的三行必须做的是关于depth buffer(深度缓存)的。将深度缓存设想为屏幕后面的层。
-    //深度缓存不断的对物体进入屏幕内部有多深进行跟踪。
-    //我们本节的程序其实没有真正使用深度缓存，但几乎所有在屏幕上显示3D场景OpenGL程序都使用深度缓存。
-    //它的排序决定那个物体先画。这样您就不会将一个圆形后面的正方形画到圆形上来。深度缓存是OpenGL十分重要的部分。
-    glClearDepth(1.0f);	// 设置深度缓存
-    glEnable(GL_DEPTH_TEST); // 启用深度测试
-    glDepthFunc(GL_LEQUAL);	// 所作深度测试的类型
+    glEnable(GL_LIGHT0);											// Enable Default Light
+    glEnable(GL_LIGHTING);											// Enable Lighting
 
-    //接着告诉OpenGL我们希望进行最好的透视修正。这会十分轻微的影响性能。但使得透视图看起来好一点。
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// 告诉系统对透视进行修正
+    glEnable(GL_COLOR_MATERIAL);									// Enable Color Material
 }
 
-//下一段包括了所有的绘图代码。任何您所想在屏幕上显示的东东都将在此段代码中出现。
-//以后的每个教程中我都会在例程的此处增加新的代码。如果您对OpenGL已经有所了解的话，您可以在glLoadIdentity()调用之后，
-//试着添加一些OpenGL代码来创建基本的形。
-//如果您是OpenGL新手，等着我的下个教程。目前我们所作的全部就是将屏幕清除成我们前面所决定的颜色，清除深度缓存并且重置场景。
-//我们仍没有绘制任何东东。
 void MyGLWidget::paintGL()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	//清除屏幕和深度缓存
-    glLoadIdentity();	// 重置当前的模型观察矩阵
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);				// Clear Screen And Depth Buffer
+    glLoadIdentity();												// Reset The Current Modelview Matrix
+    glTranslatef(-1.5f,0.0f,-6.0f);									// Move Left 1.5 Units And Into The Screen 6.0
+
+    glPushMatrix();													// NEW: Prepare Dynamic Transform
+    glMultMatrixf(Transform.M);										// NEW: Apply Dynamic Transform
+    glColor3f(0.75f,0.75f,1.0f);
+    torus(0.30f,1.00f);
+    glPopMatrix();													// NEW: Unapply Dynamic Transform
+
+    glLoadIdentity();												// Reset The Current Modelview Matrix
+    glTranslatef(1.5f,0.0f,-6.0f);									// Move Right 1.5 Units And Into The Screen 7.0
+
+    glPushMatrix();													// NEW: Prepare Dynamic Transform
+    glMultMatrixf(Transform.M);										// NEW: Apply Dynamic Transform
+    glColor3f(1.0f,0.75f,0.75f);
+    gluSphere(m_quadratic,1.3f,20,20);
+    glPopMatrix();													// NEW: Unapply Dynamic Transform
+    glFlush ();														// Flush The GL Rendering Pipeline
 }
 
 void MyGLWidget::keyPressEvent(QKeyEvent *event)
@@ -132,6 +223,76 @@ void MyGLWidget::keyPressEvent(QKeyEvent *event)
             break;
         }
     }
+    QGLWidget::keyPressEvent(event);
+}
+
+void MyGLWidget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::RightButton)
+    {
+        Matrix3fSetIdentity(&LastRot);								// Reset Rotation
+        Matrix3fSetIdentity(&ThisRot);								// Reset Rotation
+        Matrix4fSetRotationFromMatrix3f(&Transform, &ThisRot);		// Reset Rotation
+        updateGL();
+    }
+    else if(event->button() == Qt::LeftButton)
+    {
+        m_mousePt.s.X = (GLfloat)(event->pos().x());
+        m_mousePt.s.Y = (GLfloat)(event->pos().y());
+        LastRot = ThisRot;// Set Last Static Rotation To Last Dynamic One
+        m_arcBall.click(&m_mousePt);// Update Start Vector And Prepare For Dragging
+        m_leftButtonClicked = true;
+    }
+    QGLWidget::mousePressEvent(event);
+}
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(m_leftButtonClicked)
+    {
+        m_mousePt.s.X = (GLfloat)(event->pos().x());
+        m_mousePt.s.Y = (GLfloat)(event->pos().y());
+
+        Quat4fT ThisQuat;
+        m_arcBall.drag(&m_mousePt, &ThisQuat);// Update End Vector And Get Rotation As Quaternion
+        Matrix3fSetRotationFromQuat4f(&ThisRot, &ThisQuat);// Convert Quaternion Into Matrix3fT
+        Matrix3fMulMatrix3f(&ThisRot, &LastRot);// Accumulate Last Rotation Into This One
+        Matrix4fSetRotationFromMatrix3f(&Transform, &ThisRot);// Set Our Final Transform's Rotation From This One
+        updateGL();
+    }
+    QGLWidget::mouseMoveEvent(event);
+}
+
+void MyGLWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    m_leftButtonClicked = false;
+    updateGL();
+    QGLWidget::mouseReleaseEvent(event);
+}
+
+void MyGLWidget::torus(float MinorRadius, float MajorRadius)					// Draw A Torus With Normals
+{
+    int i, j;
+    glBegin( GL_TRIANGLE_STRIP );									// Start A Triangle Strip
+        for (i=0; i<20; i++ )										// Stacks
+        {
+            for (j=-1; j<20; j++)									// Slices
+            {
+                float wrapFrac = (j%20)/(float)20;
+                float phi = PI2*wrapFrac;
+                float sinphi = float(sin(phi));
+                float cosphi = float(cos(phi));
+
+                float r = MajorRadius + MinorRadius*cosphi;
+
+                glNormal3f(float(sin(PI2*(i%20+wrapFrac)/(float)20))*cosphi, sinphi, float(cos(PI2*(i%20+wrapFrac)/(float)20))*cosphi);
+                glVertex3f(float(sin(PI2*(i%20+wrapFrac)/(float)20))*r,MinorRadius*sinphi,float(cos(PI2*(i%20+wrapFrac)/(float)20))*r);
+
+                glNormal3f(float(sin(PI2*(i+1%20+wrapFrac)/(float)20))*cosphi, sinphi, float(cos(PI2*(i+1%20+wrapFrac)/(float)20))*cosphi);
+                glVertex3f(float(sin(PI2*(i+1%20+wrapFrac)/(float)20))*r,MinorRadius*sinphi,float(cos(PI2*(i+1%20+wrapFrac)/(float)20))*r);
+            }
+        }
+    glEnd();														// Done Torus
 }
 
 //在这一课中，我已试着尽量详细解释一切。每一步都与设置有关，并创建了一个全屏OpenGL程序。
